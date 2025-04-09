547acdde9622e58c968192d448ee27734501f150
Cédric Baumann <cedric.baumann94@gmail.com>
2025-04-09 10:16:01 +0200

    ajdusted Database
    - made a new roleconcept
    - updated database.ts, schema.ts and seeding
    - implemented error page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent], // ❌ LoginComponent entfernen
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
-    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
